a66086ee161afb66f53bb7d4ab6391d71039ca2f
Jorge Luis Mayorga <jl.mayorga236@uniandes.edu.co>
2017-05-18 13:23:03 -0500

    oli

--- zPID-Test/main2.py
+++ zPID-Test/main2.py
@@ -31,68 +31,74 @@ SETTINGS_FILE = "RTIMULib"
 # ---- Define Function 
 def computeHeight(pressure):
     return 44330.8 * (1 - pow(pressure / 1013.25, 0.190263));
+def init_Board():
+    print "HI! Welcome to Dron Uniandes Dev & Resch"
+    print  "......."
+    print  "Initing ADC Pot..."
+    ADC.setup()
+    print  "Initing PWM Ports for P19_14...P19_22"
+    PWM.start("P9_14", 50,500, 0)
+    PWM.start("P9_16", 50,500, 0)
+    PWM.start("P9_21", 50,500, 0)
+    PWM.start("P9_22", 50,500, 0)
+
+    PWM.set_duty_cycle("P9_14", 50.5)
+    PWM.set_duty_cycle("P9_16", 50.5)
+    PWM.set_duty_cycle("P9_21", 50.5)
+    PWM.set_duty_cycle("P9_22", 50.5)
+
+    print "Waiting one second ...."
+    time.sleep(0.5)
+    print "Thats it, ready to go"
+    PWM.set_duty_cycle("P9_14", 60.5)
+    PWM.set_duty_cycle("P9_16", 60.5)
+    PWM.set_duty_cycle("P9_21", 60.5)
+    PWM.set_duty_cycle("P9_22", 60.5)
 # ----------------------------------------------------------- #
 
 
 # ----------------------------------------------------------- #
 # ---------- Init IMU --------------------------------------- #
+def init_IMU(SETTINGS_FILE):
+    print("Using settings file " + SETTINGS_FILE + ".ini")
+    if not os.path.exists(SETTINGS_FILE + ".ini"):
+        print("Settings file does not exist, will be created")
 
-print("Using settings file " + SETTINGS_FILE + ".ini")
-if not os.path.exists(SETTINGS_FILE + ".ini"):
-    print("Settings file does not exist, will be created")
+    s = RTIMU.Settings(SETTINGS_FILE)
+    imu = RTIMU.RTIMU(s)
+    pressure = RTIMU.RTPressure(s)
 
-s = RTIMU.Settings(SETTINGS_FILE)
-imu = RTIMU.RTIMU(s)
-pressure = RTIMU.RTPressure(s)
+    print("IMU Name: " + imu.IMUName())
+    print("Pressure Name: " + pressure.pressureName())
 
-print("IMU Name: " + imu.IMUName())
-print("Pressure Name: " + pressure.pressureName())
+    if (not imu.IMUInit()):
+        print("IMU Init Failed")
+        sys.exit(1)
+    else:
+        print("IMU Init Succeeded");
 
-if (not imu.IMUInit()):
-    print("IMU Init Failed")
-    sys.exit(1)
-else:
-    print("IMU Init Succeeded");
+    # this is a good time to set any fusion parameters
 
-# this is a good time to set any fusion parameters
+    imu.setSlerpPower(0.02)
+    imu.setGyroEnable(True)
+    imu.setAccelEnable(True)
+    imu.setCompassEnable(True)
 
-imu.setSlerpPower(0.02)
-imu.setGyroEnable(True)
-imu.setAccelEnable(True)
-imu.setCompassEnable(True)
+    if (not pressure.pressureInit()):
+        print("Pressure sensor Init Failed")
+    else:
+        print("Pressure sensor Init Succeeded")
+        poll_interval = 1
+        poll_interval = imu.IMUGetPollInterval()
+        print("Recommended Poll Interval: %dmS\n" % poll_interval)
+    return (imu,poll_interval)
 
-if (not pressure.pressureInit()):
-    print("Pressure sensor Init Failed")
-else:
-    print("Pressure sensor Init Succeeded")
-poll_interval = 1
-poll_interval = imu.IMUGetPollInterval()
-print("Recommended Poll Interval: %dmS\n" % poll_interval)
 
+
+init_Board()
+[imu,poll_interval] = init_IMU()    
 # --------------------------------------------------------------------------- #
-print "HI! Welcome to Dron Uniandes Dev & Resch"
 
-print  "......."
-print  "Initing ADC Pot..."
-ADC.setup()
-print  "Initing PWM Ports for P19_14...P19_22"
-PWM.start("P9_14", 50,500, 0)
-PWM.start("P9_16", 50,500, 0)
-PWM.start("P9_21", 50,500, 0)
-PWM.start("P9_22", 50,500, 0)
-
-PWM.set_duty_cycle("P9_14", 50.5)
-PWM.set_duty_cycle("P9_16", 50.5)
-PWM.set_duty_cycle("P9_21", 50.5)
-PWM.set_duty_cycle("P9_22", 50.5)
-
-print "Waiting one second ...."
-time.sleep(poll_interval*1.0/1000.0)
-print "Thats it, ready to go"
-PWM.set_duty_cycle("P9_14", 60.5)
-PWM.set_duty_cycle("P9_16", 60.5)
-PWM.set_duty_cycle("P9_21", 60.5)
-PWM.set_duty_cycle("P9_22", 60.5)
                    
 
 valPitch = 0
