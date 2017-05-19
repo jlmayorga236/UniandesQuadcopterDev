d37f564769c764edd7bdd4c300c12abedf8fa8ad
Jorge Luis Mayorga <jl.mayorga236@uniandes.edu.co>
2017-05-19 08:00:48 -0500

    p

--- DroneQuadControl/main.py
+++ DroneQuadControl/main.py
@@ -65,10 +65,11 @@ class ThreadControl (threading.Thread):
         global M3 
         global M4
         while True:
-            M1 = max(-10,min(10,0  - 0.15*(0 - Roll)))
-            M2 = max(-10,min(10,0  + 0.15*(0 - Roll)))
-            M3 = max(-10,min(10,0  - 0.15*(0 - Roll)))
-            M4 = max(-10,min(10,0  + 0.15*(0 - Roll)))
+            
+            M1 = max(-10,min(10,0  + 0.2*(0 - Roll)))
+            M2 = max(-10,min(10,0  - 0.2*(0 - Roll)))
+            M3 = max(-10,min(10,0  + 0.2*(0 - Roll)))
+            M4 = max(-10,min(10,0  - 0.2*(0 - Roll)))
             SetMotorsPWM(70,M1,M2,M3,M4)
             print " "
             print("M1: %f M2: %f M3: %f M4: %f" % (M1,M2,M3,M4))
