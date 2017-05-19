21fefdda78fdbb42cc119f05e099a3dcbd661d62
Jorge Luis Mayorga <jl.mayorga236@uniandes.edu.co>
2017-05-19 00:37:49 -0500

    p

--- DroneQuadControl/main.py
+++ DroneQuadControl/main.py
@@ -9,6 +9,7 @@
 import random
 import time
 import math
+import threading
 from pyBBBDrone import *
 # -------------------------------------------------- #
 
@@ -26,6 +27,67 @@ Yaw = 0.0
 # -------------------------------------------------- #
 
 
+
+
+
+# -------------------------------------------------- # 
+# ---  Threads   ----------------------------------- #
+# -------------------------------------------------- #
+class ThreadIMU (threading.Thread):
+    def __init__(self, threadID, name,IMUData):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+    def run(self):
+        global Pitch
+        global Roll
+        global Yaw
+        while True:
+            if IMUData.IMURead():
+                x, y, z = IMUData.getFusionData()
+                Pitch = math.degrees(x - Pitch0) -3
+                Roll = math.degrees(y - Roll0) +2
+                Yaw = math.degrees(z - Yaw0) -150
+                
+                print("%f %f %f" % (Pitch,Roll,Yaw))
+                
+class ThreadHTTP (threading.Thread):
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+    def run(self):
+        global Pitch
+        global Roll
+        global Yaw
+        global M1
+        global M2
+        global M3
+        global M4
+        valx = 0
+        valy = 0
+        valz = 0
+        while True:
+            payload = {'Pitch': Pitch,'Roll': Roll,'Yaw': Yaw,'x': valx,'y': valy,'z': valz}
+            try:
+                r = requests.get("http://drone.ias-uniandes.com/setParameters_Quadcopter.php/get", params=payload,timeout=5,headers={'Connection': 'close'})
+                rjson= r.json()
+                jsonThrottle = float(rjson['Throttle'])
+                jsonM1 = float(rjson['M1'])
+                jsonM2 = float(rjson['M2'])
+                jsonM3 = float(rjson['M3'])
+                jsonM4 = float(rjson['M4'])
+                r = requests.session()
+                r.keep_alive = False
+            except Exception,e:
+            print "HTTP Get ERROR :("
+            
+            
+# -------------------------------------------------- #
+
+
+
+
 # -------------------------------------------------- #
 # --- Main Thread ---------------------------------- #
 # -------------------------------------------------- #
@@ -36,12 +98,11 @@ Init_Board()
 
 [Pitch0,Roll0,Yaw0] = GetInitialAngles(IMUData)
 
-while True:
-    if IMUData.IMURead():
-            x, y, z = IMUData.getFusionData()
-            Pitch = math.degrees(x - Pitch0) -3
-            Roll = math.degrees(y - Roll0) +2
-            Yaw = math.degrees(z - Yaw0) -3
-            print("%f %f %f" % (Pitch,Roll,Yaw))
- 
-            # -------------------------------------------------- #
+# Create new threads
+thread1 = ThreadIMU(1, "IMU DAQ", IMUData)
+thread2 = ThreadHTTP(2, "HTTP Control")
+
+# Start new Threads
+thread1.start()
+thread2.start()
+# -------------------------------------------------- #
