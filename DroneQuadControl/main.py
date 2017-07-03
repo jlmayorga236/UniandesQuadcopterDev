25bc8849d4586d8da04cc412ad839563aef7d0d2
Jorge Luis Mayorga <jl.mayorga236@uniandes.edu.co>
2017-07-03 18:53:42 -0500

    oli

--- DroneQuadControl/main.py
+++ DroneQuadControl/main.py
@@ -94,7 +94,7 @@ class ThreadIMU (threading.Thread):
 		        Pitch=Pitchm/5
 		        Roll = Rollm/5
 		        Yaw = Yawm/5
-		    z = ADC.read("P9_40")
+		    z = -397*ADC.read("P9_40")+166
 		    print z
 		    line1 = str(Pitch) + " , " +str(Roll) + " , "+str(Yaw) + " , " + str(z)
 		    line = line1.split(",")
